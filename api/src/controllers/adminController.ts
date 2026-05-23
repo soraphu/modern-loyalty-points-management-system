9f52924774972b78b7effe3a02f555ac8fa11b12
soraphu <soraphu@gmail.com>
2026-05-23 20:09:36 +0700

    feat: Generate Points Token - pass test.

--- api/src/controllers/adminController.ts
+++ api/src/controllers/adminController.ts
@@ -5,6 +5,7 @@ import { OwnerService } from '../services/ownerService';
 import { ApiResponse } from '../utils/apiResponse';
 import { Auth } from '../services/authService';
 import { AdminRoles } from '../generated/prisma/enums';
+import { StaffService } from '../services/staffService';
 
 export interface AdminTokenPayload {
     id: string;
@@ -46,11 +47,25 @@ export async function adminLoginController(request: FastifyRequest, reply: Fasti
 
 export async function generatePointsTokenController(request: FastifyRequest, reply: FastifyReply) {
     try {
+        const jsonBody: any = request.body;
+
+        Validation.requiredFields(jsonBody, ['points']);
+
         const accessToken = Validation.requireAuthHeader(request);
 
+        const decodePayload: any = Auth.verifyAndDecodeToken(accessToken);
+        logs.info('Decode Payload: ', decodePayload);
+
+        const admin: any = await Auth.lowestAllowRole({ adminId: decodePayload.id, lowestAllowRole: 'MANAGER' });
+        logs.info('Admin: ', admin);
+
+        const codeString = await StaffService.generatePointsToken(admin.id, jsonBody.points);
+        logs.success('Code String: ', codeString);
+
         const res = ApiResponse.success({
-            statusCode: 200,
-            msg: 'Login successfully.',
+            statusCode: 201,
+            msg: 'Generate points token success.',
+            data: { code_string: codeString }
         });
 
         return reply.status(res.statusCode).send(res.payload);
