2a636350030674de535290b4f0f1a47edbdb99e4
soraphu <soraphu@gmail.com>
2026-05-28 14:43:28 +0700

    feat: Finish setup admins and customers frontend side.

--- admins/src/App.tsx
+++ admins/src/App.tsx
@@ -1,8 +1,17 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import { Button } from './components/ui/button';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Button /> },
+]);
+
 function App() {
 
   return (
-    <>
-    </>
+    <main>
+      <RouterProvider router={router} />
+    </main>
   )
 }
 
--- customers/src/App.tsx
+++ customers/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import Home from './pages/Home';
