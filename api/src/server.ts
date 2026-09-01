5a3cea87338c723f68ce3ada4813097b93929620
soraphu <soraphu@gmail.com>
2026-09-01 19:32:57 +0700

    fix: Got blocked by cors.

--- api/src/server.ts
+++ api/src/server.ts
@@ -34,7 +34,9 @@ const fastifyOptions = {
 
 const allowedOrigins = [
     'http://localhost:5173',
-    'http://localhost:5174'
+    'http://localhost:5174',
+    'http://127.0.0.1:5173',
+    'http://127.0.0.1:5174',
 ];
 
 const apiPrefix = CONFIG.API_PREFIX;
